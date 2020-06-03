1883123bcf71186d850de88e7d333eb14e3ebffb
Zach Proctor <z.proctor@hotmail.com>
2020-06-02 21:04:15 -0600

    still having handlebar issues

--- public:assets/js/script.js
+++ public:assets/js/script.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
     
-    $(".devour-form").on("submit", function(event) {
-      event.preventDefault();
-  
+    // $(".devour-form").on("submit", function(event) {
+      $("#text-devour-button").on("click", function(event) {
+
+      // event.preventDefault();
+        alert("Hi")
       var burger_id = $(this).children(".burger_id").val();
       console.log(burger_id);
-      $.ajax({
-        method: "PUT",
-        url: "/burgers/" + burger_id
-      }).then(function(data) {
-        location.reload();
-      });
+      // $.ajax({
+      //   method: "PUT",
+      //   url: "/burgers/" + burger_id
+      // }).then(function(data) {
+      //   location.reload();
+      // });
   
     });
   });
