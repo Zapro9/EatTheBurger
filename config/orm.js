971997c5988e6ba619efdb12569133b0731a06c1
Zach Proctor <z.proctor@hotmail.com>
2020-06-02 19:19:40 -0600

    Can add burgers but I can't remove them yet

--- config/orm.js
+++ config/orm.js
@@ -11,13 +11,10 @@ function printQuestionMarks(num) {
 }
 
 function objToSql(ob) {
-  // column1=value, column2=value2,...
   var arr = [];
-
   for (var key in ob) {
     arr.push(key + "=" + ob[key]);
   }
-
   return arr.toString();
 }
 
@@ -31,8 +28,7 @@ var orm = {
       cb(result);
     });
   },
-  // vals is an array of values that we want to save to cols
-  // cols are the columns we want to insert the values into
+
   create: function(table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
@@ -43,8 +39,6 @@ var orm = {
     queryString += printQuestionMarks(vals.length);
     queryString += ") ";
 
-    console.log(queryString);
-
     connection.query(queryString, vals, function(err, result) {
       if (err) {
         throw err;
@@ -52,8 +46,7 @@ var orm = {
       cb(result);
     });
   },
-  // objColVals would be the columns and values that you want to update
-  // an example of objColVals would be {name: panther, sleepy: true}
+
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
@@ -62,7 +55,6 @@ var orm = {
     queryString += " WHERE ";
     queryString += condition;
 
-    console.log(queryString);
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,26 +1,26 @@
 var express = require("express");
+
 var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-    res.redirect("/burgers");
+  res.redirect("/burgers");
 });
 
-router.get("/burgers", function(req,res) {
-    burger.all(function(burgerData) {
-        res.render("index", {burger_data: burgerData});
-    });
+router.get("/burgers", function(req, res) {
+  burger.all(function(burgerData) {
+    res.render("index", { burger_data: burgerData });
+  });
 });
 
-router.post("/api/burgers", function(req, res) {
-    burger.create(req.body.burger_name, function(res) {
-        res.redirect("/");
-    });
+router.post("/burgers/create", function(req, res) {
+  burger.create(req.body.burger_name, function(result) {
+    res.redirect("/");
+  });
 });
 
 router.put("/burgers/:id", function(req, res) {
   burger.update(req.params.id, function(result) {
-    console.log(result);
     res.sendStatus(200);
   });
 });
